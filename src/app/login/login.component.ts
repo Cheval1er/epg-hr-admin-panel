aeb904cf4b05b3d653adf81f5137809c1cfcb701
nutsa.matchavariani <nmatchavariani@cu.edu.ge>
2022-07-25 13:26:30 +0400

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { fadeInUp400ms } from 'src/@vex/animations/fade-in-up.animation';
+
+
+@Component({
+  selector: 'vex-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    fadeInUp400ms
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  form: UntypedFormGroup;
+
+  inputType = 'password';
+  visible = false;
+
+  constructor(private router: Router,
+    private fb: UntypedFormBuilder,
+    private cd: ChangeDetectorRef,
+    private snackbar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  send() {
+    this.router.navigate(['/']);
+
+  }
+
+  onSubmit() {
+    this.form.reset();
+
+  }
+
+  toggleVisibility() {
+    if (this.visible) {
+      this.inputType = 'password';
+      this.visible = false;
+      this.cd.markForCheck();
+    } else {
+      this.inputType = 'text';
+      this.visible = true;
+      this.cd.markForCheck();
+    }
+  }
+}
