6bc98cec1e8919289585529ada031dbfe759e9c6
nutsa.matchavariani <nmatchavariani@cu.edu.ge>
2022-08-09 13:05:56 +0400

    b

--- src/app/pages/user-search/user-search.module.ts
+++ src/app/pages/user-search/user-search.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+
 import { UserSearchComponent } from './user-search.component';
 import { UserSearchRoutingModule } from './user-search-routing.module';
 
