05dff9001eabb2b3010a9ec135bae417ac50e01c
nutsa.matchavariani <nmatchavariani@cu.edu.ge>
2022-08-09 16:11:19 +0400

    added search and reset button in user search reset button works

--- src/app/pages/user-search/user-search.component.ts
+++ src/app/pages/user-search/user-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { resetExtensions } from 'showdown';
 
 @Component({
   selector: 'vex-user-search',
@@ -12,7 +13,7 @@ export class UserSearchComponent implements OnInit {
   @ViewChild('paginator') paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  displayedColumns = ['id', 'username', 'firstname', 'lastname', 'mobile', 'status'];
+  displayedColumns: string[] = ['checked', 'id', 'username', 'firstname', 'lastname', 'mobile', 'status'];
   groupedColumns: string[] = ['grouped'];
 
   thirdGroupedColumns: string[] = ['edit'];
@@ -25,6 +26,16 @@ export class UserSearchComponent implements OnInit {
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
+
+  }
+  searchField;
+  searchfield2;
+  clearSearchField() {
+    this.searchField = '';
+    this.searchfield2 = '';
+  }
+  onReset() {
+    this.onReset()
 
   }
 }
